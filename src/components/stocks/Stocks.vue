a9fc3d2fdecf848a88f9360ffb4782406805a632
D'ontreye Nero <dontreyenero@gmail.com>
2017-03-22 08:44:22 -0400

    Add vuex to manage state

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<app-stock v-for="stock in stocks"></app-stock>
+		<app-stock v-for="stock in stocks" :stock="stock"></app-stock>
 	</div>
 </template>
 
@@ -8,18 +8,13 @@
 	import Stock from './Stock.vue';
 
 	export default {
-		data() {
-			return {
-				stocks: [
-					{id: 1, name: 'UPS', price: 110},
-					{id: 1, name: 'Google', price: 200},
-					{id: 1, name: 'Apple', price: 250},
-					{id: 1, name: 'Twitter', price: 8}
-				]
-			}
-		},
 		components: {
 			appStock: Stock
+		},
+		computed: {
+			stocks() {
+				return this.$store.getters.stocks
+			}
 		}
 	}
 </script>
