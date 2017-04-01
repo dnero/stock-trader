ec801abf3389cb1a00a3fed7587938906b494387
D'ontreye Nero <dontreyenero@gmail.com>
2017-04-01 07:53:54 -0400

    Enable buying/selling

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,3 +1,24 @@
 <template>
-	<h1>The Portfolio Component</h1>
+	<div>
+		<app-stock v-for="stock in stocks"></app-stock>
+	</div>
 </template>
+
+<script>
+	import {mapGetters} from 'vuex';
+	import Stock from './Stock.vue';
+
+	export default {
+		computed: {
+			stocks() {
+				return this.$store.getters.stockPortfolio;
+			}
+			// mapGetters({
+			// 	stocks: 'stockPortfolio'
+			// })
+		},
+		components: {
+			appStock: Stock
+		}
+	}
+</script>
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="col-sm-6 col-md-4">
-		<div class="panel panel-success">
+		<div class="panel panel-info">
 			<div class="panel-heading">
 				<h3 class="panel-title">
 					{{ stock.name }}
-					<small>(Price: {{ stock.price}})</small>
+					<small>(Price: {{ stock.price}}) | Quantity: {{ stock.quantity }})</small>
 				</h3>
 			</div>
 			<div class="panel-body">
@@ -18,10 +18,10 @@
 				<div class="pull-right">
 					<button	
 						class="btn btn-success"
-						@click="buyStock"
+						@click="_sellStock"
 						:disabled="quantity <= 0"
-					>Buy</button>
-					{{ quantity }}
+					>Sell
+					</button>
 				</div>
 			</div>
 		</div>
@@ -29,6 +29,8 @@
 </template>
 
 <script>
+	import {mapActions} from 'vuex';
+
 	export default {
 		props: ['stock'],
 		data() {
@@ -37,14 +39,18 @@
 			}
 		},
 		methods: {
-			buyStock() {
+			// ...mapActions({
+			// 	placeSellOrder: 'sellStock'
+			// }),
+			_sellStock() {
 				const order = {
 					stockId: this.stock.id,
 					stockPrice: this.stock.price,
 					quantity: this.quantity
 				};
-				console.log(order);
+				this._sellStock(order);
 				this.quantity = 0;
+				store.dispatch('sellStock');
 			}
 		}
 	}
